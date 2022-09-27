508031880c5751fb3fafc4f9d0cf7e70d64f6767
jaimorsl <jaortizs@sura.com.co>
2022-09-27 13:29:51 -0500

    Proyecto maquetado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wc-alert';
+  @Input() titulo: string = 'Alerta';
+  @Input() isopen: string = 'false';
+  @Output('submit') mensaje = new EventEmitter<boolean>();
+
+  onClick() {
+    this.mensaje.emit(false);
+  }
+
+  isOpenModal() {
+    return this.isopen === 'true';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  exports: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('wc-alert', el);
+  }
+
+  ngDoBootstrap() {}
+}
